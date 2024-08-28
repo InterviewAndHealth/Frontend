948c7ce560a9d8262be0b448e23e074c525c97c1
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-08-28 20:38:34 +0530

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Home from "./Home";
+
+export { Home, Login, Register };
---
+++ src/routes/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Login } from "../pages";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
