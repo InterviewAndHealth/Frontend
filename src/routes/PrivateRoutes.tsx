df74125afb99495bddbcd4fe41e8c2f2cb469e07
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-10-02 20:22:09 +0530

    fix: routing bug

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -2,16 +2,26 @@ import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
 import { useStudentProfile } from "@/services/user/queries";
 import { Loading } from "@/components/custom/Loading";
+import { jwtDecode } from "jwt-decode";
+import Notify from "@/lib/notify";
 
 export default function PrivateRoutes({ children }: { children: ReactNode }) {
   const { data, isPending } = useStudentProfile();
   const authToken = localStorage.getItem("auth-token");
-  console.log(data);
 
   if (authToken != null) {
-    if(isPending){
-      return <Loading/>;
-    }else{
+    const decodedToken = jwtDecode(authToken);
+    const currentTime = Date.now() / 1000;
+    if (decodedToken && decodedToken.exp) {
+      if (decodedToken.exp < currentTime) {
+        Notify("info", "Session expired!!");
+        localStorage.removeItem("auth-token");
+        return <Navigate to="/login" />;
+      }
+    }
+    if (isPending) {
+      return <Loading />;
+    } else {
       if (data) {
         return <>{children}</>;
       } else {
